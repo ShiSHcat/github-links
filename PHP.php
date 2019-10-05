7a24721b76d602ec9dce120951191a19a363ee19
ShiSHcat <51242428+ShiSHcat@users.noreply.github.com>
2019-10-05 14:46:28 +0200

    fix

--- PHP.php
+++ PHP.php
@@ -54,6 +54,7 @@ function get($user,$repo,$skip_readme=true)  {
             $p = implode("/",$p_ar);
           $fs = urlencode($s_p);
             $fs = str_replace("%2F","/",$fs);
+            $fs = str_replace("+","%20",$fs);
             $link = 'https://github.com/'.urlencode($user).'/'.urlencode($repo).'/raw/master/'.$fs;
            if (strpos($s_p,"/") === false) {
             $return[$filename] = $link;
